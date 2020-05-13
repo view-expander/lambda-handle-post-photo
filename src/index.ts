b51155486719336d2d50ad1a2b22803a3926fbbb
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-05-13 18:54:51 +0900

    feat: demo code .

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyResult, S3Event } from 'aws-lambda'
 
-export async function handler(): Promise<APIGatewayProxyResult> {
+export async function handler(event: S3Event): Promise<APIGatewayProxyResult> {
+  console.log('an event received:', event)
+
   return {
     statusCode: 200,
     body: JSON.stringify({}),
